c8c337257d2dec42df65f13de8a2ac57b8fe30c1
Kandasamy Chokkalingam <samchoks@hotmail.com>
2021-03-05 14:51:24 -0500

    Initialize

---
+++ Robot.java
@@ -0,0 +1,6 @@
+public class Robot extends TimedRobot {
+    // These fields have been created for you, but you'll need to add an assignment
+    private TalonSRX intake;
+    private CANSparkMax shooter;
+    private CANSparkMax shooterFollower;
+}
